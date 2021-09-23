e145bee7d855319004e91af02e972bb19de06cd4
Jesús S <jesus.sanchez.asd@gmail.com>
2021-09-22 21:42:19 -0500

    Agregando variable a pass db

--- config/db.js
+++ config/db.js
@@ -2,10 +2,15 @@ const { Sequelize } = require("sequelize");
 //Extraer valores de variables.env
 require("dotenv").config({ path: "variables.env" });
 
-const db = new Sequelize(process.env.BD_NOMBRE, process.env.BD_USER, "", {
-  host: process.env.BD_HOST,
-  dialect: "mysql",
-});
+const db = new Sequelize(
+  process.env.BD_NOMBRE,
+  process.env.BD_USER,
+  process.env.BD_PASS,
+  {
+    host: process.env.BD_HOST,
+    dialect: "mysql",
+  }
+);
 
 module.exports = db;
 
